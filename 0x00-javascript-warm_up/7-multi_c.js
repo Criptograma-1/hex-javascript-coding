c54a4cdb67a974cd980f688ef7b62a0ba744ea6d
Criptograma-1 <andresa.info@gmail.com>
2021-12-20 13:39:32 +0000

    Add task 7

---
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+let x = Number(argv[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (x > 0) {
+    console.log('C is fun');
+    x--;
+}}
