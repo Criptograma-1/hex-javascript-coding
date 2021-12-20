fb47b99fbe841ba65a69a2ee9a307bb8b69ae098
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-12-20 10:29:48 -0300

    Fix

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let str_array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const str_array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 str_array.forEach((val, index) => {
   console.log(val);
 });
