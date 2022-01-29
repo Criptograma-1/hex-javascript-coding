dbfd7c75b6ab0c75d0ca5dd8478be748c9dd4e89
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-01-29 20:33:02 -0300

    Update 1-writeme.js

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -4,8 +4,8 @@ const argv = process.argv;
 const file = argv[2];
 const text = argv[3];
 
-fs.writeFile(file, text, 'utf8', function (err) {
-  if (err) {
-    return console.log(err);
-  }
-});
+try {
+  fs.writeFile(file, text) 
+} catch (err) {
+  console.log(err);
+}
