2037e665223b52b7e74854855c17f7d949a92ab9
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-01-29 20:24:52 -0300

    Change to arrow function

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -3,10 +3,7 @@ const argv = process.argv;
 const id = argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 const request = require('request');
-request(url, function (err, response, body) {
-  if (err) {
-    console.error('error:', err);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+request(url, (err, response, body) => {
+  if (err) console.error('error:', err);
+  console.log(JSON.parse(body).title);
 });
