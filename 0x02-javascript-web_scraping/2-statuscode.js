ff097a3a16339cca7bc980f2cb0b9e692948baec
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-01-29 20:26:09 -0300

    Change to arrow function

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -2,9 +2,8 @@
 const argv = process.argv;
 const url = argv[2];
 const request = require('request');
-request(url, function (err, response, body) {
-  if (err) {
-    return console.log(err);
-  }
+
+request(url, (err, response, body) => {
+  if (err) console.log(err);
   console.log('code:', response && response.statusCode);
 });
