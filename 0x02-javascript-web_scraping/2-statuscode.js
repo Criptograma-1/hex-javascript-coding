bc7ce12ab975cb9c36968115e201bee104a7fbd4
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-01-29 18:37:38 -0300

    Add error handler

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@ const argv = process.argv;
 const url = argv[2];
 const request = require('request');
 request(url, function (err, response, body) {
+  if (err) {
+    return console.log(err);
+  }
   console.log('code:', response && response.statusCode);
 });
