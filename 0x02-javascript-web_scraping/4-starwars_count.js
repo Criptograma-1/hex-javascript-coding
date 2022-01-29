76043bfc6a7073a5b8057c512483c3d87a9dd64b
Criptograma-1 <andresa.info@gmail.com>
2022-01-29 22:08:01 +0000

    Add task 4

---
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const argv = process.argv;
+const id = '18';
+const url = argv[2];
+const request = require('request');
+request(url, function (err, response, body) {
+  if (err) {
+    console.error('error:', err);
+  } else {
+    const bresponse = JSON.parse(body).results;
+    let count = 0;
+    for (const i of bresponse) {
+      for (const j of i.characters) {
+        if (j.search(id) > 0) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
