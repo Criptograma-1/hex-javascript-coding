c3db1b9b76c4dd15d6a118edf4f7a9d141557a55
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-01-29 20:16:42 -0300

    Change for arrow function

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -2,20 +2,18 @@
 const argv = process.argv;
 const url = argv[2];
 const request = require('request');
-request(url, function (err, response, body) {
-  if (err) {
-    console.error('error:', err);
-  } else {
-    const bresponse = JSON.parse(body);
-    const user = {};
-    for (const i of bresponse) {
-      if (i.completed === true) {
-        if (user[i.userId] === undefined) {
-          user[i.userId] = 0;
-        }
-        user[i.userId] += 1;
+
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  const bresponse = JSON.parse(body);
+  const user = {};
+  for (const i of bresponse) {
+    if (i.completed === true) {
+      if (user[i.userId] === undefined) {
+        user[i.userId] = 0;
       }
+      user[i.userId] += 1;
     }
-    console.log(user);
   }
+  console.log(user);
 });
