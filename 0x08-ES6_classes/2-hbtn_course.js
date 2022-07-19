f64bc6d5fe8ea62d4050f6ac2f82326351e32b85
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-19 18:42:53 -0300

    Update 2-hbtn_course.js

--- 0x08-ES6_classes/2-hbtn_course.js
+++ 0x08-ES6_classes/2-hbtn_course.js
@@ -1,20 +1,12 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
     return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
   }
 
   set name(name) {
@@ -25,12 +17,20 @@ export default class HolbertonCourse {
     }
   }
 
+  get length() {
+    return this._length;
+  }
+
   set length(length) {
     if (typeof length === 'number') {
       this._length = length;
     } else {
       throw new TypeError('Length must be a number');
     }
+  }
+
+  get students() {
+    return this._students;
   }
 
   set students(students) {
