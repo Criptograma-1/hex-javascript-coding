d19f6d5868095a1104cb01c2e7009d827dca171f
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-19 18:41:40 -0300

    Update 3-currency.js

--- 0x08-ES6_classes/3-currency.js
+++ 0x08-ES6_classes/3-currency.js
@@ -1,27 +1,23 @@
 export default class Currency {
-  constructor(name, code) {
-    this._name = name;
-    this._code = code;
-  }
-
-  get name() {
-    return this._name;
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    }
+  set code(code) {
+    if (typeof code === 'string') this._code = code;
   }
 
-  set code(code) {
-    if (typeof code === 'string') {
-      this._code = code;
-    }
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') this._name = name;
   }
 
   displayFullCurrency() {
