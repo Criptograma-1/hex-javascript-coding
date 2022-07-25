2fcf5bb8848516c63f36e6bd73d3522b0adb9741
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-25 16:32:00 -0300

    Update 2-get_students_by_loc.js

--- 0x09-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x09-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,6 +2,6 @@ export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) {
     return [];
   }
-  
+
   return students.filter((student) => student.location === city);
 }
