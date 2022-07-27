0371098a8afe39245fcfd3e106155b66d869f8db
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-27 17:23:22 -0300

    Update index.js

--- 0x0A-Node_JS_basic/full_server/routes/index.js
+++ 0x0A-Node_JS_basic/full_server/routes/index.js
@@ -1,4 +1,3 @@
-const { response } = require('express');
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const StudentsController = require('../controllers/StudentsController');
