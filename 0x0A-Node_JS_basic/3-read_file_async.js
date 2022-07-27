4da6746e894653457e71c5ca3db3a25945c33de4
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-27 16:16:44 -0300

    Add files via upload

--- 0x0A-Node_JS_basic/3-read_file_async.js
+++ 0x0A-Node_JS_basic/3-read_file_async.js
@@ -1,38 +1,60 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 
-async function countStudents(path) {
-  try {
-    const lines = await (await fs.readFile(path, { encoding: 'utf8' })).split(/\r?\n/);
-    lines.shift();
-    let countStudents = 0;
-    const printAux = {};
-    for (const line of lines) {
-      if (line !== '') {
-        countStudents += 1;
-        const [firstName, lastName, age, field] = line.split(','); // eslint-disable-line
-        if (!printAux[field]) {
-          printAux[field] = {
-            students: 1,
-            studentsNames: [firstName],
-          };
-        } else {
-          const newCount = printAux[field].students + 1;
-          const newStudents = (printAux[field].studentsNames).concat(firstName);
-          printAux[field] = {
-            students: newCount,
-            studentsNames: newStudents,
-          };
-        }
+function countStudents(path) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, results) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = results.split('\n');
+        let i = 0;
+        let countStudents = 0;
+        let msg = '';
+        const fields = {};
+
+        const getLines = () => {
+          for (const line of lines) {
+            if (line.trim() !== '' && i > 0) {
+              countStudents += 1;
+              const [fname, lname, age, field] = line.split(','); // eslint-disable-line
+              if (!fields[field]) {
+                fields[field] = {
+                  count: 1,
+                  students: [fname],
+                };
+              } else {
+                const newCount = fields[field].count + 1;
+                const newStudents = fields[field].students.concat(fname);
+                fields[field] = {
+                  count: newCount,
+                  students: newStudents,
+                };
+              }
+            }
+            i += 1;
+          }
+        };
+
+        const display = async () => {
+          getLines();
+          console.log(`Number of students: ${countStudents}`);
+          msg += `Number of students: ${countStudents}\n`;
+          for (const field of Object.keys(fields)) {
+            const n = fields[field].count;
+            const names = fields[field].students.join(', ');
+            console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+            msg += `Number of students in ${field}: ${n}. List: ${names}\n`;
+          }
+          msg = msg.slice(0, -1);
+        };
+
+        display();
+        resolve(msg);
       }
-    }
-    console.log(`Number of students: ${countStudents}`);
-    for (const field of Object.keys(printAux)) {
-      const numberOfStudents = printAux[field].students;
-      const names = printAux[field].studentsNames.join(', ');
-      console.log(`Number of students in ${field}: ${numberOfStudents}. List: ${names}`);
-    }
-  } catch (error) {
-    console.log(error);
-  }
+    });
+  });
+
+  return promise;
 }
+
 module.exports = countStudents;
