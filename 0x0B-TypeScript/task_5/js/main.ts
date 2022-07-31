291114352ff4782512778174c8b480bb5d1ccc86
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-31 17:57:06 -0300

    Add files via upload

--- 0x0B-TypeScript/task_5/js/main.ts
+++ 0x0B-TypeScript/task_5/js/main.ts
@@ -1,19 +1,17 @@
-type UUID = number;
-
 export interface MajorCredits {
-  credit: number;
-  brand: UUID;
+	_brand: 'major';
+	credit: number;
 }
 
 export interface MinorCredits {
-  credit: number;
-  brand: UUID;
+	_brand: 'minor';
+	credit: number;
 }
 
-export function sumMajorCredits (subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return { credit: subject1.credit + subject2.credit } as MajorCredits
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+	return { credit: subject1.credit + subject2.credit } as MajorCredits;
 }
 
-export function sumMinorCredits (subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return { credit: subject1.credit + subject2.credit } as MinorCredits
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+	return { credit: subject1.credit + subject2.credit } as MinorCredits;
 }
