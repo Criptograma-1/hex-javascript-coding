9b6ee465bd55d60c83279cab4d45a0d20a6cb8a1
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-31 16:21:28 -0300

    Add files via upload

--- 0x0B-TypeScript/task_4/js/main.ts
+++ 0x0B-TypeScript/task_4/js/main.ts
@@ -1,4 +1,6 @@
 ///<reference path ='./subjects/Cpp.ts' />
+///<reference path ='./subjects/Java.ts' />
+///<reference path ='./subjects/React.ts' />
 const cpp: Subjects.Cpp = new Subjects.Cpp();
 const java: Subjects.Java = new Subjects.Java();
 const react: Subjects.React = new Subjects.React();
