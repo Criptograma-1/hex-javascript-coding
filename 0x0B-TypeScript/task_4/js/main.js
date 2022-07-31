07e897605697a8b1d8c79b80d35b46f30fb037c3
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-31 17:40:24 -0300

    Add files via upload

--- 0x0B-TypeScript/task_4/js/main.js
+++ 0x0B-TypeScript/task_4/js/main.js
@@ -1,36 +1,32 @@
+"use strict";
+exports.__esModule = true;
+exports.cTeacher = exports.react = exports.java = exports.cpp = void 0;
 ///<reference path ='./subjects/Cpp.ts' />
 ///<reference path ='./subjects/Java.ts' />
 ///<reference path ='./subjects/React.ts' />
 /// <reference path="./subjects/Teacher.ts" />
 /// <reference path="./subjects/Subject.ts" />
-const cpp: Subjects.Cpp = new Subjects.Cpp();
-const java: Subjects.Java = new Subjects.Java();
-const react: Subjects.React = new Subjects.React();
-
-let cTeacher = {
-	firstName: 'Guillaume',
-	lastName: 'Salva',
-	experienceTeachingC: 10,
+var cpp = new Subjects.Cpp();
+exports.cpp = cpp;
+var java = new Subjects.Java();
+exports.java = java;
+var react = new Subjects.React();
+exports.react = react;
+var cTeacher = {
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+    experienceTeachingC: 10
 };
-
+exports.cTeacher = cTeacher;
 console.log('C++');
 cpp.setTeacher = cTeacher;
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
-
 console.log('Java');
 java.setTeacher = cTeacher;
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
-
 console.log('React');
 react.setTeacher = cTeacher;
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
-
-export {
-	cpp,
-	java,
-	react,
-	cTeacher,
-}
