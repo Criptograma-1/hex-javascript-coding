6804215db50345b8ed55d5ed72c405f8590cdb87
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-08-02 14:15:16 -0300

    Add files via upload

--- 0x0B-TypeScript/task_4/js/main.ts
+++ 0x0B-TypeScript/task_4/js/main.ts
@@ -3,11 +3,11 @@
 ///<reference path ='./subjects/React.ts' />
 /// <reference path="./subjects/Teacher.ts" />
 /// <reference path="./subjects/Subject.ts" />
-const cpp: Subjects.Cpp = new Subjects.Cpp();
-const java: Subjects.Java = new Subjects.Java();
-const react: Subjects.React = new Subjects.React();
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
 
-const cTeacher: Subjects.TeacherInterface = {
+export const cTeacher: Subjects.TeacherInterface = {
 	firstName: 'Guillaume',
 	lastName: 'Salva',
 	experienceTeachingC: 10,
@@ -27,10 +27,3 @@ console.log('React');
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
-
-export {
-	cpp,
-	java,
-	react,
-	cTeacher,
-}
